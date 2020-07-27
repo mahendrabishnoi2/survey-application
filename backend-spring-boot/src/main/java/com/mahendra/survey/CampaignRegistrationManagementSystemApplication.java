413ff44c39b033755d6152cf44ab8446c7385fbf
Mahendra Bishnoi <mahendrabishnoi2@gmail.com>
2020-07-27 11:57:36 +0530

    initialization of spring boot and angular projects

---
+++ backend-spring-boot/src/main/java/com/mahendra/survey/CampaignRegistrationManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mahendra.survey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignRegistrationManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignRegistrationManagementSystemApplication.class, args);
+	}
+
+}
---
+++ backend-spring-boot/src/test/java/com/mahendra/survey/CampaignRegistrationManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahendra.survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignRegistrationManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
